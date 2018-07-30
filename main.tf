b466aecdb7ad7f2bea0007d39a1366ea872ca7bc
Daniel Hartnell <danielhartnell@gmail.com>
2018-07-30 16:10:32 -0700

    start a single cluster for now

--- main.tf
+++ main.tf
@@ -2,12 +2,8 @@
 # EKS cluster management
 #---
 
-module "eks-cluster-prod" {
-  source       = "./modules/eks-cluster"
-  cluster-name = "kubernetes-prod"
-}
-
 module "eks-cluster-development" {
   source       = "./modules/eks-cluster"
   cluster-name = "kubernetes-development"
 }
+
