9d7e5736898b296ee8588467fa4f83b6b2fb0b44
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2018-09-17 15:48:11 +0200

    Fetch the zone_id given the zone name

--- infrastructure/ses.tf
+++ infrastructure/ses.tf
@@ -1,5 +1,9 @@
+data "aws_route53_zone" "iam-mozilla" {
+  name         = "iam.mozilla.com."
+}
+
 module "ses" {
   source  = "./modules/ses"
   domain  = "alerts.iam.mozilla.com"
-  zone_id = "..."
+  zone_id = "${data.aws_route53_zone.iam-mozilla.zone_id}"
 }
