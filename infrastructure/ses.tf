231c3373c0ddb6b5bd3c29d822b78dd0f5b36987
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2018-09-20 16:55:01 +0200

    changing the domain name after further discussion

--- infrastructure/ses.tf
+++ infrastructure/ses.tf
@@ -4,6 +4,6 @@ data "aws_route53_zone" "iam-mozilla" {
 
 module "ses" {
   source  = "./modules/ses"
-  domain  = "prometheus.iam.mozilla.com"
+  domain  = "ops.iam.mozilla.com"
   zone_id = "${data.aws_route53_zone.iam-mozilla.zone_id}"
 }
