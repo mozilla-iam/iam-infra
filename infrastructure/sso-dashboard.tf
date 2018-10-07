5e7f196a296e9d12a31e9d44358657b1e51f4dbf
Daniel Hartnell <danielhartnell@gmail.com>
2018-10-07 16:08:51 -0700

    add prod and stage pipelines for SSO dashboard

---
+++ infrastructure/sso-dashboard.tf
@@ -0,0 +1,27 @@
+#---
+# Create CI pipeline:
+#   - CodeBuild and ECR
+#   - Uses OAuth to create webhooks in GitHub repositories
+#
+# https://docs.aws.amazon.com/codepipeline/latest/userguide/GitHub-authentication.html
+#---
+
+module "sso-dashboard-ci-stage" {
+  source = "./modules/ci"
+
+  project_name   = "sso-dashboard"
+  environment    = "stage"
+  github_repo    = "https://github.com/mozilla-iam/sso-dashboard"
+  github_branch  = "master"
+  enable_webhook = "true"
+}
+
+module "sso-dashboard-ci-prod" {
+  source = "./modules/ci"
+
+  project_name   = "sso-dashboard"
+  environment    = "prod"
+  github_repo    = "https://github.com/mozilla-iam/sso-dashboard"
+  github_branch  = "production"
+  enable_webhook = "true"
+}
