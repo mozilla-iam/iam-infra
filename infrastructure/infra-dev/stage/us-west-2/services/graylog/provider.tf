f3677e9555511aa7a1b8601bf3fa4217cadd2cdd
Daniel Hartnell <danielhartnell@gmail.com>
2018-12-04 22:11:23 -0500

    many changes to change Terraform organization and functionality

---
+++ infrastructure/infra-dev/stage/us-west-2/services/graylog/provider.tf
@@ -0,0 +1,17 @@
+#---
+# Provider Configuration
+#---
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+terraform {
+  required_version = "~> 0.11"
+
+  backend "s3" {
+    bucket = ""
+    key    = "stage/us-west-2/services/graylog/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
---
+++ infrastructure/infra-dev/stage/us-west-2/vpc/main.tf
@@ -0,0 +1,29 @@
+#---
+# Provides a VPC for the stage environment
+#---
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "1.46.0"
+
+  name = "kubernetes-${var.environment}-${var.region}"
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "${data.aws_availability_zones.available.names[0]}",
+    "${data.aws_availability_zones.available.names[1]}",
+    "${data.aws_availability_zones.available.names[2]}",
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  private_subnets = ["10.0.0.0/19", "10.0.64.0/19", "10.0.128.0/19"]
+  public_subnets  = ["10.0.32.0/19", "10.0.96.0/19", "10.0.160.0/19"]
+
+  tags = {
+    "Environment"                                                       = "${var.environment}"
+    "kubernetes.io/cluster/kubernetes-${var.environment}-${var.region}" = "shared"
+    "k8s.io/cluster-autoscaler/enabled"                                 = "true"
+  }
+}
