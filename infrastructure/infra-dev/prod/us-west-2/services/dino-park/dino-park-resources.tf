5b599d602d139939f0cf860fd2ba3976410f1747
Daniel Hartnell <danielhartnell@gmail.com>
2019-01-10 15:10:07 -0800

    adding Dino Park resources to new TF structure

---
+++ infrastructure/infra-dev/prod/us-west-2/services/dino-park/dino-park-resources.tf
@@ -0,0 +1,60 @@
+#---
+# Elasticsearch
+#---
+
+data "aws_caller_identity" "current" {}
+
+data "aws_security_group" "default" {
+  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+  name   = "default"
+}
+
+resource "aws_elasticsearch_domain" "es" {
+  domain_name           = "dinopark-mozillians-es"
+  elasticsearch_version = "6.3"
+
+  ebs_options {
+    ebs_enabled = true
+    volume_type = "gp2"
+    volume_size = 10
+  }
+
+  cluster_config {
+    instance_count           = 3
+    instance_type            = "t2.small.elasticsearch"
+    dedicated_master_enabled = false
+    zone_awareness_enabled   = false
+  }
+
+  snapshot_options {
+    automated_snapshot_start_hour = 17
+  }
+
+  vpc_options {
+    subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets[0]}"]
+    security_group_ids = ["${data.aws_security_group.default.id}"]
+  }
+
+  tags {
+    Domain  = "dinopark-mozillians-es"
+    app     = "elasticsearch"
+    env     = "dinopark"
+    project = "mozillians"
+  }
+
+  access_policies = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::320464205386:role/dino-park-staging"
+      },
+      "Action": "es:*",
+      "Resource": "arn:aws:es:us-west-2:320464205386:domain/dinopark-mozillians-es/*"
+    }
+  ]
+}
+CONFIG
+}
