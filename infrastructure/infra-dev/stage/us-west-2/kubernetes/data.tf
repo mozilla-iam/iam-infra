57f304aa830df57bea1c29dfb0269c1cfa5baf42
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-01-10 16:32:24 +0100

    Add bucket name where it was missing

--- infrastructure/infra-dev/stage/us-west-2/kubernetes/data.tf
+++ infrastructure/infra-dev/stage/us-west-2/kubernetes/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config {
-    bucket = ""
+    bucket = "eks-terraform-shared-state"
     key    = "stage/us-west-2/vpc/terraform.tfstate"
     region = "us-west-2"
   }
