03da6c9ea845f091c00eff8c7936e44a8a283265
Daniel Hartnell <danielhartnell@gmail.com>
2019-01-15 19:41:52 -0800

    adding secondary worker group to enable blue green deployments for nodes

--- infrastructure/infra-dev/prod/us-west-2/kubernetes/main.tf
+++ infrastructure/infra-dev/prod/us-west-2/kubernetes/main.tf
@@ -8,8 +8,8 @@ locals {
 
   worker_groups = [
     {
-      name                  = "k8s-worker-blue"
-      ami_id                = "ami-0f54a2f7d2e9c88b3"
+      name                  = "k8s-worker-green"
+      ami_id                = "ami-0a2abab4107669c1b"
       asg_desired_capacity  = "3"
       asg_max_size          = "10"
       asg_min_size          = "3"
@@ -19,20 +19,19 @@ locals {
       root_volume_size      = "100"
       subnets               = "${join(",", data.terraform_remote_state.vpc.private_subnets)}"
     },
-  ] #,
-
-  # {
-  #   name = "k8s-worker-green"
-  #   ami_id = "ami-0f54a2f7d2e9c88b3"
-  #   asg_desired_capacity = "0"
-  #   asg_max_size = "0"
-  #   asg_min_size = "0"
-  #   autoscaling_enabled = true
-  #   protect_from_scale_in = true
-  #   instance_type = "m4.large"
-  #   root_volume_size = "100"
-  #   subnets = "${join(",", data.terraform_remote_state.vpc.private_subnets)}"
-  # }
+    {
+      name                  = "k8s-worker-blue"
+      ami_id                = "ami-0a2abab4107669c1b"
+      asg_desired_capacity  = "0"
+      asg_max_size          = "0"
+      asg_min_size          = "0"
+      autoscaling_enabled   = true
+      protect_from_scale_in = true
+      instance_type         = "m4.large"
+      root_volume_size      = "100"
+      subnets               = "${join(",", data.terraform_remote_state.vpc.private_subnets)}"
+    },
+  ]
 
   tags = {
     "Environment" = "${var.environment}"
@@ -44,10 +43,11 @@ module "eks" {
   version = "1.7.0"
 
   cluster_name       = "${local.cluster_name}"
+  cluster_version    = "1.11"
   subnets            = ["${data.terraform_remote_state.vpc.private_subnets}"]
   vpc_id             = "${data.terraform_remote_state.vpc.vpc_id}"
   worker_groups      = "${local.worker_groups}"
-  worker_group_count = "1"
+  worker_group_count = "2"
   tags               = "${local.tags}"
   write_kubeconfig   = "false"
 }
