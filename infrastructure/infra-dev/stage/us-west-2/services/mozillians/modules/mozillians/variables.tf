f2734e0374b2b3f1bdd9019b230a4e3be314bfec
Daniel Hartnell <danielhartnell@gmail.com>
2019-01-25 15:50:37 -0800

    adding more Mozillians resources to new Terraform

---
+++ infrastructure/infra-dev/stage/us-west-2/services/mozillians/modules/mozillians/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "environment" {}
+variable "elasticache_redis_instance_size" {}
+variable "elasticache_memcached_instance_size" {}
+variable "cis_publisher_role_arn" {}
+variable "rds_instance_class" {}
+variable "k8s_source_security_group" {}
