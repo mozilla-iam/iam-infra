cb15de7b7b45a4c4fee0223106c45dc95d209897
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-02-01 11:08:51 +0100

    Graylog behind Auth0 using mozilla-oidc-proxy

--- infrastructure/infra-dev/prod/us-west-2/services/graylog/data.tf
+++ infrastructure/infra-dev/prod/us-west-2/services/graylog/data.tf
@@ -19,3 +19,12 @@ data "terraform_remote_state" "kubernetes" {
     region = "us-west-2"
   }
 }
+
+data "aws_route53_zone" "infra_iam" {
+  name = "infra.iam.mozilla.com."
+}
+
+data "aws_elb" "k8s-elb" {
+  name = "a00435690f99111e8989b0ace417809a"
+}
+
