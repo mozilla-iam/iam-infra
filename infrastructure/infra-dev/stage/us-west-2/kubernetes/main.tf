fc42a8f3e60f913b1d1b88f49aeaa24d13b9e775
eziegenhorn <eric@ziegenhorns.com>
2019-02-13 09:42:45 -0600

    Update stage to use new EKS AMI for CVE-2019-5736

--- infrastructure/infra-dev/stage/us-west-2/kubernetes/main.tf
+++ infrastructure/infra-dev/stage/us-west-2/kubernetes/main.tf
@@ -9,10 +9,10 @@ locals {
   worker_groups = [
     {
       name                  = "k8s-worker-blue"
-      ami_id                = "ami-0f54a2f7d2e9c88b3"
-      asg_desired_capacity  = "3"
-      asg_max_size          = "10"
-      asg_min_size          = "3"
+      ami_id                = "ami-0e36fae01a5fa0d76"
+      asg_desired_capacity  = "0"
+      asg_max_size          = "0"
+      asg_min_size          = "0"
       autoscaling_enabled   = true
       protect_from_scale_in = true
       instance_type         = "m4.large"
@@ -21,10 +21,10 @@ locals {
     },
     {
       name                  = "k8s-worker-green"
-      ami_id                = "ami-0f54a2f7d2e9c88b3"
-      asg_desired_capacity  = "0"
-      asg_max_size          = "0"
-      asg_min_size          = "0"
+      ami_id                = "ami-0e36fae01a5fa0d76"
+      asg_desired_capacity  = "3"
+      asg_max_size          = "10"
+      asg_min_size          = "3"
       autoscaling_enabled   = true
       protect_from_scale_in = true
       instance_type         = "m4.large"
