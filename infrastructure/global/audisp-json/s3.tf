e26f338b2fbc4bf8daa425f9f4062cfc0a05191b
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-03-05 14:39:56 +0100

    Enable audisp-json in staging

---
+++ infrastructure/global/audisp-json/s3.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket" "audisp-json" {
+  bucket = "audisp-json"
+  acl    = "private"
+
+  tags {
+    Name   = "audisp-json"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_account_ro" {
+  bucket = "${aws_s3_bucket.audisp-json.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "Allow account members to get objects",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { 
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::audisp-json/*"
+    }
+  ]
+}
+POLICY
+}
