b7f3ec4a14fca879c7b2e7a52d4f6a775197a5b4
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-03-06 16:37:25 +0100

    add environment name to policy

--- infrastructure/stage/us-west-2/services/ssm-agent/main.tf
+++ infrastructure/stage/us-west-2/services/ssm-agent/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy_attachment" "attach_ssm_policy_to_ec2" {
-  name       = "policy-for-ssm"
+  name       = "policy-for-ssm-stage"
   roles      = ["kubernetes-stage-us-west-220190207165215030100000005"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
