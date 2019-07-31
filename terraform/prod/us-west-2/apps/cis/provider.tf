d42346deff86aec10891b47a9b9b82fa064e93b3
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-07-31 11:03:41 +0200

    Create DBs for CIS, allowing connections from inside k8s

---
+++ terraform/prod/us-west-2/apps/cis/provider.tf
@@ -0,0 +1,17 @@
+#---
+# Provider Configuration
+#---
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_version = "~> 0.11"
+
+  backend "s3" {
+    bucket = "eks-terraform-shared-state"
+    key    = "prod/us-west-2/services/cis/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
---
+++ terraform/prod/us-west-2/apps/cis/psql.tf
@@ -0,0 +1,92 @@
+resource "aws_db_instance" "cis-prod" {
+  identifier                  = "cis-vault-identity-prod"
+  allocated_storage           = 5
+  max_allocated_storage       = 20
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "11"
+  instance_class              = "db.t2.micro"
+  username                    = "cis"
+  password                    = "oneTimePassword"
+  parameter_group_name        = "default.postgres11"
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  db_subnet_group_name        = "${aws_db_subnet_group.cis-prod-db.name}"
+  skip_final_snapshot         = "true"
+  vpc_security_group_ids      = ["${aws_security_group.allow-psql.id}"]
+}
+
+resource "aws_db_subnet_group" "cis-prod-db" {
+  name       = "cis-prod-db-subnet"
+  subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets}"]
+}
+
+resource "aws_db_instance" "cis-stage" {
+  identifier                  = "cis-vault-identity-stage"
+  allocated_storage           = 5
+  max_allocated_storage       = 20
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "11"
+  instance_class              = "db.t2.micro"
+  username                    = "cis"
+  password                    = "oneTimePassword"
+  parameter_group_name        = "default.postgres11"
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  db_subnet_group_name        = "${aws_db_subnet_group.cis-stage-db.name}"
+  skip_final_snapshot         = "true"
+  vpc_security_group_ids      = ["${aws_security_group.allow-psql.id}"]
+}
+
+resource "aws_db_subnet_group" "cis-stage-db" {
+  name       = "cis-stage-db-subnet"
+  subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets}"]
+}
+
+resource "aws_db_instance" "cis-dev" {
+  identifier                  = "cis-vault-identity-dev"
+  allocated_storage           = 5
+  max_allocated_storage       = 20
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "11"
+  instance_class              = "db.t2.micro"
+  username                    = "cis"
+  password                    = "oneTimePassword"
+  parameter_group_name        = "default.postgres11"
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  db_subnet_group_name        = "${aws_db_subnet_group.cis-dev-db.name}"
+  skip_final_snapshot         = "true"
+  vpc_security_group_ids      = ["${aws_security_group.allow-psql.id}"]
+}
+
+resource "aws_db_subnet_group" "cis-dev-db" {
+  name       = "cis-dev-db-subnet"
+  subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets}"]
+}
+
+resource "aws_security_group" "allow-psql" {
+  name        = "allow_psql_from_k8s_workers"
+  description = "Allow traffic to PSQL from Kubernetes prod workers"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = ["${data.terraform_remote_state.k8s.worker_security_group_id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_psql_from_k8s_prod_workers"
+  }
+}
