f182c61e045cd7b6f8cb3d4f183fb1d3b0ce22f7
Florian Merz <flomerz@gmail.com>
2019-08-13 18:53:53 +0200

    Add picture api dns for dev evn.

--- terraform/stage/us-west-2/apps/dinopark/route53.tf
+++ terraform/stage/us-west-2/apps/dinopark/route53.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "dinopark" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "picture_api" {
+  zone_id = "${data.aws_route53_zone.sso_allizom_org.zone_id}"
+  name    = "pictures.api.dev.sso.allizom.org"
+  type    = "A"
+
+  alias {
+    name                   = "dualstack.${data.aws_elb.k8s-elb.dns_name}"
+    zone_id                = "${data.aws_elb.k8s-elb.zone_id}"
+    evaluate_target_health = false
+  }
+}
