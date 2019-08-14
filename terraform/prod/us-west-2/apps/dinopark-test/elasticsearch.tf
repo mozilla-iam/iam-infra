392d5687a5bdc8ba39e5849053311d53dcc9db4d
Florian Merz <flomerz@gmail.com>
2019-08-14 16:04:09 +0200

    remove upgrade TODO comments for dinopark

--- terraform/prod/us-west-2/apps/dinopark-test/elasticsearch.tf
+++ terraform/prod/us-west-2/apps/dinopark-test/elasticsearch.tf
@@ -24,14 +24,6 @@ resource "aws_elasticsearch_domain" "dinopark-es" {
   }
 
   vpc_options {
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibilty in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     subnet_ids = [data.terraform_remote_state.vpc.outputs.private_subnets[0]]
 
     #security_group_ids = ["${aws_security_group.allow_https_from_kubernetes.id}"]
