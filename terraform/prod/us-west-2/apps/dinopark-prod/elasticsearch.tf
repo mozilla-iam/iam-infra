dace0fd5d85646e9ac2b3e02b05bc599efbcac9c
Florian Merz <flomerz@gmail.com>
2019-08-14 15:44:37 +0200

    update dinopark terraform to 12 + picture api

--- terraform/prod/us-west-2/apps/dinopark-prod/elasticsearch.tf
+++ terraform/prod/us-west-2/apps/dinopark-prod/elasticsearch.tf
@@ -24,15 +24,23 @@ resource "aws_elasticsearch_domain" "dinopark-es" {
   }
 
   vpc_options {
-    subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets[0]}"]
-    security_group_ids = ["${data.aws_security_group.es-allow-https.id}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    subnet_ids         = [data.terraform_remote_state.vpc.outputs.private_subnets[0]]
+    security_group_ids = [data.aws_security_group.es-allow-https.id]
   }
 
-  tags {
+  tags = {
     Domain  = "dinopark-es"
     app     = "elasticsearch"
-    env     = "${var.environment}"
-    region  = "${var.region}"
+    env     = var.environment
+    region  = var.region
     project = "dinopark"
   }
 
@@ -55,5 +63,6 @@ resource "aws_elasticsearch_domain" "dinopark-es" {
   ]
 }
 CONFIG
+
 }
 
--- terraform/prod/us-west-2/apps/dinopark-test/elasticsearch.tf
+++ terraform/prod/us-west-2/apps/dinopark-test/elasticsearch.tf
@@ -24,16 +24,25 @@ resource "aws_elasticsearch_domain" "dinopark-es" {
   }
 
   vpc_options {
-    subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets[0]}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    subnet_ids = [data.terraform_remote_state.vpc.outputs.private_subnets[0]]
+
     #security_group_ids = ["${aws_security_group.allow_https_from_kubernetes.id}"]
-    security_group_ids = ["${data.aws_security_group.es-allow-https.id}"]
+    security_group_ids = [data.aws_security_group.es-allow-https.id]
   }
 
-  tags {
+  tags = {
     Domain  = "dinopark-es"
     app     = "elasticsearch"
-    env     = "${var.environment}"
-    region  = "${var.region}"
+    env     = var.environment
+    region  = var.region
     project = "dinopark"
   }
 
@@ -56,5 +65,6 @@ resource "aws_elasticsearch_domain" "dinopark-es" {
   ]
 }
 CONFIG
+
 }
 
