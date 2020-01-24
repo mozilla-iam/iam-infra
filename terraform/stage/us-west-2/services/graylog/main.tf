b6c5786e03b399d1a27df0a61bef6b1de8360973
Alberto del Barrio <adelbarrio@mozilla.com>
2020-01-24 16:04:28 +0100

    remove terraform 12 todos

--- terraform/stage/us-west-2/services/graylog/main.tf
+++ terraform/stage/us-west-2/services/graylog/main.tf
@@ -15,14 +15,6 @@ resource "aws_security_group" "allow_https_from_kubernetes" {
     from_port = 0
     to_port   = 443
     protocol  = "tcp"
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     security_groups = [data.terraform_remote_state.kubernetes.outputs.worker_security_group_id]
   }
 }
@@ -51,14 +43,6 @@ resource "aws_elasticsearch_domain" "graylog" {
   }
 
   vpc_options {
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     subnet_ids         = [data.terraform_remote_state.vpc.outputs.private_subnets[0]]
     security_group_ids = [aws_security_group.allow_https_from_kubernetes.id]
   }
