431a3acae4e95b6b247e2406a0b126e5d0cb628a
Alberto del Barrio <adelbarrio@mozilla.com>
2020-05-14 14:58:52 +0200

    Upgrade stage cluster to EKS 1.15

--- terraform/stage/us-west-2/ssm-agent.tf
+++ terraform/stage/us-west-2/ssm-agent.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy_attachment" "attach_ssm_policy_to_ec2" {
   name       = "policy-for-ssm-stage"
-  roles      = ["kubernetes-stage-us-west-220190207165215030100000005"]
+  roles      = ["kubernetes-stage-us-west-220190207165215030100000005", "kubernetes-prod-us-west-220181206181410238800000005"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
