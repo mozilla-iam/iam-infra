2eab5d53cc05b3212291261c0d95400bb5156032
Alberto del Barrio <adelbarrio@mozilla.com>
2020-05-18 16:15:41 +0200

    Upgrade prod to EKS 1.15

--- terraform/prod/us-west-2/eks.tf
+++ terraform/prod/us-west-2/eks.tf
@@ -9,27 +9,27 @@ locals {
   worker_groups = [
     {
       name                  = "k8s-worker-green"
-      ami_id                = "ami-0c13bb9cbfd007e56"
-      asg_desired_capacity  = "0"
-      asg_max_size          = "0"
-      asg_min_size          = "0"
-      autoscaling_enabled   = true
-      protect_from_scale_in = true
-      instance_type         = "m5.large"
-      root_volume_size      = "100"
-      subnets               = module.vpc.private_subnets
-      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
-    },
-    {
-      name                  = "k8s-worker-blue"
-      ami_id                = "ami-0c13bb9cbfd007e56"
+      ami_id                = "ami-065418523a44331e5"
       asg_desired_capacity  = "5"
       asg_max_size          = "10"
       asg_min_size          = "3"
       autoscaling_enabled   = true
       protect_from_scale_in = true
       instance_type         = "m5.large"
-      root_volume_size      = "100"
+      root_volume_size      = "150"
+      subnets               = module.vpc.private_subnets
+      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
+    },
+    {
+      name                  = "k8s-worker-blue"
+      ami_id                = "ami-065418523a44331e5"
+      asg_desired_capacity  = "0"
+      asg_max_size          = "0"
+      asg_min_size          = "0"
+      autoscaling_enabled   = true
+      protect_from_scale_in = true
+      instance_type         = "m5.large"
+      root_volume_size      = "150"
       subnets               = module.vpc.private_subnets
       additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
     },
@@ -45,7 +45,7 @@ module "eks" {
 	version = "8.0.0"
 
   cluster_name          = local.cluster_name
-  cluster_version       = "1.14"
+  cluster_version       = "1.15"
   subnets               = module.vpc.private_subnets
   vpc_id                = module.vpc.vpc_id
   worker_groups         = local.worker_groups
