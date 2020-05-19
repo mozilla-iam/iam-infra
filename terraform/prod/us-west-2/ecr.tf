2462679dc9a74ee963d6aa8c3403792ea1313cb1
Alberto del Barrio <adelbarrio@mozilla.com>
2020-05-19 11:46:32 +0200

    Move dinopark-metrics to ECR

--- terraform/prod/us-west-2/ecr.tf
+++ terraform/prod/us-west-2/ecr.tf
@@ -13,3 +13,11 @@ resource "aws_ecr_repository" "graylog-oidc-proxy" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "dinopark-metrics" {
+  name = "dinopark-metrics"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
