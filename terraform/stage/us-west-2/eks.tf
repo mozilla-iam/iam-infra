a3862941d8bc368b5f7242e3c4cccab415cd94bf
Alberto del Barrio <The-smooth-operator@users.noreply.github.com>
2020-05-25 15:57:56 +0200

    Upgrade EKS module to latest version (#277)

--- terraform/stage/us-west-2/eks.tf
+++ terraform/stage/us-west-2/eks.tf
@@ -41,18 +41,69 @@ locals {
 }
 
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-	version = "8.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "12.0.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.15"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-  worker_groups   = local.worker_groups
-  #worker_group_count    = "2"
-  tags             = local.tags
-  write_kubeconfig = "false"
-  #write_aws_auth_config = "false"
+  cluster_name       = local.cluster_name
+  cluster_version    = "1.15"
+  subnets            = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  worker_groups      = local.worker_groups
+  tags               = local.tags
+  write_kubeconfig   = "false"
   manage_aws_auth = "false"
 }
 
+### Autoscaling policies
+resource "aws_iam_role_policy_attachment" "workers_autoscaling" {
+  policy_arn = aws_iam_policy.worker_autoscaling.arn
+  role       = module.eks.worker_iam_role_name
+}
+
+resource "aws_iam_policy" "worker_autoscaling" {
+  name_prefix = "eks-worker-autoscaling-${module.eks.cluster_id}"
+  description = "EKS worker node autoscaling policy for cluster ${module.eks.cluster_id}"
+  policy      = data.aws_iam_policy_document.worker_autoscaling.json
+}
+
+data "aws_iam_policy_document" "worker_autoscaling" {
+  statement {
+    sid    = "eksWorkerAutoscalingAll"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "eksWorkerAutoscalingOwn"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${module.eks.cluster_id}"
+      values   = ["owned"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+      values   = ["true"]
+    }
+  }
+}
