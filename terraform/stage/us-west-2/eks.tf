7ccf4d8767e37fee4554226869ab027392d577de
Alberto del Barrio <adelbarrio@mozilla.com>
2020-05-26 13:17:59 +0200

    Use managed nodes in Stage

--- terraform/stage/us-west-2/eks.tf
+++ terraform/stage/us-west-2/eks.tf
@@ -6,35 +6,6 @@
 locals {
   cluster_name = "kubernetes-${var.environment}-${var.region}"
 
-  worker_groups = [
-    {
-      name                  = "k8s-worker-blue"
-      ami_id                = "ami-065418523a44331e5"
-      asg_desired_capacity  = "0"
-      asg_max_size          = "0"
-      asg_min_size          = "0"
-      autoscaling_enabled   = true
-      protect_from_scale_in = true
-      instance_type         = "m5.large"
-      root_volume_size      = "150"
-      subnets               = module.vpc.private_subnets
-      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
-    },
-    {
-      name                  = "k8s-worker-green"
-      ami_id                = "ami-065418523a44331e5"
-      asg_desired_capacity  = "3"
-      asg_max_size          = "10"
-      asg_min_size          = "3"
-      autoscaling_enabled   = true
-      protect_from_scale_in = true
-      instance_type         = "m5.large"
-      root_volume_size      = "150"
-      subnets               = module.vpc.private_subnets
-      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
-    },
-  ]
-
   tags = {
     "Environment" = var.environment
   }
@@ -48,12 +19,34 @@ module "eks" {
   cluster_version    = "1.15"
   subnets            = module.vpc.private_subnets
   vpc_id             = module.vpc.vpc_id
-  worker_groups      = local.worker_groups
   tags               = local.tags
   write_kubeconfig   = "false"
   manage_aws_auth = "false"
 }
 
+# Managed nodes
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = local.cluster_name
+  node_group_name = "${local.cluster_name}_worker"
+  node_role_arn   = module.eks.worker_iam_role_arn
+  subnet_ids      = module.vpc.private_subnets
+  instance_types  = ["m5.large"]
+  disk_size       = 150
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 10
+    min_size     = 3
+  }
+
+  labels = {
+    node            = "managed"
+    node_group_name = "${local.cluster_name}_worker"
+  }
+  tags = {
+    Name = "iam-stage-eks-node"
+  }
+}
 ### Autoscaling policies
 resource "aws_iam_role_policy_attachment" "workers_autoscaling" {
   policy_arn = aws_iam_policy.worker_autoscaling.arn
