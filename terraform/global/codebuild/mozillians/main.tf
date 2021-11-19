c793a7f9a4b55271cb15ba47a301833dd82b40f4
Brett Kochendorfer <bkochendorfer@mozilla.com>
2021-11-19 10:14:15 -0600

    Completely remove mozillians

--- terraform/global/codebuild/mozillians/main.tf
+++
@@ -1,9 +0,0 @@
-module "mozillians-staging-ci" {
-  source       = "./modules/sites/mozillians-staging"
-  service_name = "mozillians-staging"
-}
-
-module "mozillians-production-ci" {
-  source       = "./modules/sites/mozillians-prod"
-  service_name = "mozillians-production"
-}
