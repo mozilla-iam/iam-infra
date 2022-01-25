b2e598527d1a942d7e8494f11cbaac2045d71ce6
Brandon Myers <pwnbus@users.noreply.github.com>
2022-01-25 09:44:32 -0600

    Replace audisp-json with generic auditd (#300)
    * Replace audisp-json with generic auditd
    
    * fix(audit.conf): Update parse directive
    
    * chore(lambda): Clean up artisanally deleted resources
    
    * chore(ami): Pull in latest AMI for CVE-2022-0185
    
    Co-authored-by: Brett Kochendorfer <bkochendorfer@mozilla.com>

--- terraform/prod/us-west-2/eks.tf
+++ terraform/prod/us-west-2/eks.tf
@@ -9,20 +9,7 @@ locals {
   worker_groups = [
     {
       name                  = "k8s-worker-green"
-      ami_id                = "ami-0adca766413605f27"
-      asg_desired_capacity  = "6"
-      asg_max_size          = "10"
-      asg_min_size          = "5"
-      autoscaling_enabled   = true
-      protect_from_scale_in = true
-      instance_type         = "m5.large"
-      root_volume_size      = "150"
-      subnets               = module.vpc.private_subnets
-      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
-    },
-    {
-      name                  = "k8s-worker-blue"
-      ami_id                = "ami-0329499d31310cf49"
+      ami_id                = "ami-0e9d7772961b84bc8"
       asg_desired_capacity  = "0"
       asg_max_size          = "0"
       asg_min_size          = "0"
@@ -31,7 +18,20 @@ locals {
       instance_type         = "m5.large"
       root_volume_size      = "150"
       subnets               = module.vpc.private_subnets
-      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/ /tmp && sudo rpm -i /tmp/audisp-json-2.2.5-1.x86_64-amazon.rpm && sudo mv /tmp/audisp-json.conf /etc/audisp/audisp-json.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
+      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/v2 /tmp && sudo mv /tmp/audit.rules /etc/audit/rules.d/audit.rules && sudo mv /tmp/auditd.conf /etc/audit/auditd.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
+    },
+    {
+      name                  = "k8s-worker-blue"
+      ami_id                = "ami-0e9d7772961b84bc8"
+      asg_desired_capacity  = "6"
+      asg_max_size          = "10"
+      asg_min_size          = "5"
+      autoscaling_enabled   = true
+      protect_from_scale_in = true
+      instance_type         = "m5.large"
+      root_volume_size      = "150"
+      subnets               = module.vpc.private_subnets
+      additional_userdata   = "aws s3 cp --recursive s3://audisp-json/v2 /tmp && sudo mv /tmp/audit.rules /etc/audit/rules.d/audit.rules && sudo mv /tmp/auditd.conf /etc/audit/auditd.conf && sudo service auditd restart && sudo yum install -y amazon-ssm-agent && sudo systemctl start amazon-ssm-agent"
     },
   ]
 
