d21537669f68b08337d3d6d41d6e9dbf301bfb83
Sven Marnach <sven@mozilla.com>
2023-05-09 09:28:57 +0200

    Update Terraform code to match external changes.

--- terraform/prod/us-west-2/graylog.tf
+++ terraform/prod/us-west-2/graylog.tf
@@ -236,16 +236,3 @@ resource "aws_cloudwatch_log_subscription_filter" "auth0_publisher_prod" {
   filter_pattern  = ""
   distribution    = "ByLogStream"
 }
-
-resource "aws_route53_record" "graylog" {
-  zone_id = data.aws_route53_zone.infra_iam.zone_id
-  name    = "graylog.infra.iam.mozilla.com"
-  type    = "A"
-
-  alias {
-    name                   = "dualstack.${data.aws_elb.k8s-elb.dns_name}"
-    zone_id                = data.aws_elb.k8s-elb.zone_id
-    evaluate_target_health = false
-  }
-}
-
