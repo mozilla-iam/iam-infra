5e9b28e2cbc8ae8d52ccc2e17908e7ace2e5b1e9
Robert Müller <rmuller@mozilla.com>
2023-05-24 08:53:01 +0200

    Update prod k8s to 1.24
    Signed-off-by: Robert Müller <rmuller@mozilla.com>

--- terraform/prod/us-west-2/eks.tf
+++ terraform/prod/us-west-2/eks.tf
@@ -16,7 +16,7 @@ module "eks" {
   version = "17.20.0"
 
   cluster_name                                       = local.cluster_name
-  cluster_version                                    = "1.23"
+  cluster_version                                    = "1.24"
   subnets                                            = module.vpc.private_subnets
   vpc_id                                             = module.vpc.vpc_id
   tags                                               = local.tags
