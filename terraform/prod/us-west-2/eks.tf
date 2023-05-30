670bc65c6958a42ddbedebf96d4d2d91a813c7da
Robert Müller <rmuller@mozilla.com>
2023-05-30 08:41:30 +0200

    update prod eks to 1.25
    Signed-off-by: Robert Müller <rmuller@mozilla.com>

--- terraform/prod/us-west-2/eks.tf
+++ terraform/prod/us-west-2/eks.tf
@@ -16,7 +16,7 @@ module "eks" {
   version = "17.20.0"
 
   cluster_name                                       = local.cluster_name
-  cluster_version                                    = "1.24"
+  cluster_version                                    = "1.25"
   subnets                                            = module.vpc.private_subnets
   vpc_id                                             = module.vpc.vpc_id
   tags                                               = local.tags
