85631e81ea20e5c318fa6ed9455814715df7ebb2
Jake Watkins <jwatkins@mozilla.com>
2024-01-31 12:21:31 -0500

    sso-dashboard cloud deploy infra

---
+++ terraform/prod/gcp-us-east1/provider.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  project     = "iam-auth0"
+  region      = "us-east1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "iam-auth0-terraform-state"
+    prefix  = "terraform/prod/gcp-us-east1/state"
+  }
+}
