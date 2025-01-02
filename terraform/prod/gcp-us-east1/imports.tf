d342778aec9aa7d4094e2533ce305b1d58ca0d61
Bheesham Persaud <bhee@mozilla.com>
2025-01-02 14:25:06 -0500

    prod Import sso-dashboard-prod resources
    Jira: IAM-1528

--- terraform/prod/gcp-us-east1/imports.tf
+++ terraform/prod/gcp-us-east1/imports.tf
@@ -17,3 +17,13 @@ import {
   id = "sso-dashboard-staging"
   to = google_compute_region_network_endpoint_group.sso_dashboard_staging
 }
+
+import {
+  id = "sso-dashboard-prod"
+  to = google_compute_backend_service.sso_dashboard_prod
+}
+
+import {
+  id = "sso-dashboard-prod"
+  to = google_compute_region_network_endpoint_group.sso_dashboard_prod
+}
