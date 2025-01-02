cfc23dccc377844b47750f9f836ecf9bba738bf0
Bheesham Persaud <bhee@mozilla.com>
2025-01-02 14:28:27 -0500

    prod Expose values which are actually set
    To better understand the difference between dev and staging I'm removing some
    default values.
    
    This should give us a better understanding of what we'd need to change, if we
    wanted to make a module for this stuff.
    
    Jira: IAM-1528

--- terraform/prod/gcp-us-east1/dev.tf
+++ terraform/prod/gcp-us-east1/dev.tf
@@ -13,9 +13,9 @@ resource "google_compute_region_network_endpoint_group" "sso_dashboard_dev" {
 }
 
 resource "google_compute_backend_service" "sso_dashboard_dev" {
-  name      = "sso-dashboard-dev"
-  port_name = "http"
-  protocol  = "HTTPS"
+  name                            = "sso-dashboard-dev"
+  port_name                       = "http"
+  protocol                        = "HTTPS"
   compression_mode                = "DISABLED"
   load_balancing_scheme           = "EXTERNAL_MANAGED"
   locality_lb_policy              = "ROUND_ROBIN"
@@ -29,9 +29,6 @@ resource "google_compute_backend_service" "sso_dashboard_dev" {
   enable_cdn = true
   cdn_policy {
     cache_mode                   = "CACHE_ALL_STATIC"
-    client_ttl                   = 3600
-    default_ttl                  = 3600
-    max_ttl                      = 86400
     signed_url_cache_max_age_sec = 0
     cache_key_policy {
       include_host         = true
