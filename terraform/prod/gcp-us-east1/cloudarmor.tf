a488fe86745f9b6a9a9fbd2ec8c9905557b35f1b
Bheesham Persaud <bhee@mozilla.com>
2025-01-02 11:48:29 -0500

    prod Use tier we're already subscribed to
    Turns out we're already subscribed to the enterprise tier (inferred from the
    starts on/expires date).
    
    Jira: IAM-1528

--- terraform/prod/gcp-us-east1/cloudarmor.tf
+++ terraform/prod/gcp-us-east1/cloudarmor.tf
@@ -2,7 +2,7 @@
 #   https://github.com/mozilla-it/webservices-infra/blob/3222c6c6151ab0af2bb8607d0629e0c537d4636f/0din/tf/modules/cloudarmor/cloudarmor.tf
 
 resource "google_compute_project_cloud_armor_tier" "default" {
-  cloud_armor_tier = "CA_ENTERPRISE_PAYGO"
+  cloud_armor_tier = "CA_ENTERPRISE_ANNUAL"
 }
 
 resource "google_compute_security_policy" "default" {
