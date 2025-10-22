42687dbc41c5ad45d9e07ad9a57c51a275160d0d
Bheesham Persaud <bhee@mozilla.com>
2025-10-22 16:11:57 -0400

    Terraform fmt
    Jira: IAM-1824

--- terraform/prod/gcp-us-east1/deploy.tf
+++ terraform/prod/gcp-us-east1/deploy.tf
@@ -1,6 +1,6 @@
 resource "google_clouddeploy_delivery_pipeline" "sso-dashboard" {
-  location = "us-east1"
-  name     = "sso-dashboard"
+  location    = "us-east1"
+  name        = "sso-dashboard"
   description = "Deployment pipeline for sso-dashboard"
 
   serial_pipeline {
@@ -22,8 +22,8 @@ resource "google_clouddeploy_delivery_pipeline" "sso-dashboard" {
 }
 
 resource "google_clouddeploy_target" "dev" {
-  location = "us-east1"
-  name     = "dev"
+  location    = "us-east1"
+  name        = "dev"
   description = "Development target"
   run {
     location = "projects/iam-auth0/locations/us-east1"
@@ -31,14 +31,14 @@ resource "google_clouddeploy_target" "dev" {
   execution_configs {
     usages            = ["RENDER", "DEPLOY"]
     execution_timeout = "3600s"
-    service_account = google_service_account.sso_dashboard.email
+    service_account   = google_service_account.sso_dashboard.email
   }
   require_approval = false
 }
 
 resource "google_clouddeploy_target" "staging" {
-  location = "us-east1"
-  name     = "staging"
+  location    = "us-east1"
+  name        = "staging"
   description = "Staging target"
   run {
     location = "projects/iam-auth0/locations/us-east1"
@@ -46,14 +46,14 @@ resource "google_clouddeploy_target" "staging" {
   execution_configs {
     usages            = ["RENDER", "DEPLOY"]
     execution_timeout = "3600s"
-    service_account = google_service_account.sso_dashboard_staging.email
+    service_account   = google_service_account.sso_dashboard_staging.email
   }
   require_approval = false
 }
 
 resource "google_clouddeploy_target" "prod" {
-  location = "us-east1"
-  name     = "prod"
+  location    = "us-east1"
+  name        = "prod"
   description = "Production target"
   run {
     location = "projects/iam-auth0/locations/us-east1"
@@ -61,7 +61,7 @@ resource "google_clouddeploy_target" "prod" {
   execution_configs {
     usages            = ["RENDER", "DEPLOY"]
     execution_timeout = "3600s"
-    service_account = google_service_account.sso_dashboard_prod.email
+    service_account   = google_service_account.sso_dashboard_prod.email
   }
   require_approval = false
 }
--- terraform/prod/gcp-us-east1/service_accounts.tf
+++ terraform/prod/gcp-us-east1/service_accounts.tf
@@ -3,69 +3,69 @@
 # It's this account that GitHub uses. See the policies in GCP for the full
 # picture.
 resource "google_service_account" "sso_dashboard" {
-  account_id                   = "sso-dashboard"
-  display_name                 = "sso-dashboard"
+  account_id   = "sso-dashboard"
+  display_name = "sso-dashboard"
 }
 
 resource "google_service_account" "sso_dashboard_staging" {
-  account_id                   = "sso-dashboard-staging"
-  description                  = "A dedicated service account for sso-dashboard-staging cloud run service"
-  display_name                 = "sso-dashboard-staging"
+  account_id   = "sso-dashboard-staging"
+  description  = "A dedicated service account for sso-dashboard-staging cloud run service"
+  display_name = "sso-dashboard-staging"
 }
 
 resource "google_service_account" "sso_dashboard_prod" {
-  account_id                   = "sso-dashboard-prod"
-  description                  = "A dedicated service account for sso-dashboard-prod cloud run service"
-  display_name                 = "sso-dashboard-prod"
+  account_id   = "sso-dashboard-prod"
+  description  = "A dedicated service account for sso-dashboard-prod cloud run service"
+  display_name = "sso-dashboard-prod"
 }
 
 resource "google_service_account" "runtime_sso_dashboard_dev" {
-  account_id                   = "sso-dashboard-dev-runtime"
-  display_name                 = "SSO Dashboard Dev (runtime)"
+  account_id   = "sso-dashboard-dev-runtime"
+  display_name = "SSO Dashboard Dev (runtime)"
 }
 
 resource "google_secret_manager_secret_iam_member" "runtime_sso_dashboard_dev_secret_key" {
   secret_id = data.google_secret_manager_secret.sso_dashboard_dev_secret_key.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = google_service_account.runtime_sso_dashboard_dev.member
+  role      = "roles/secretmanager.secretAccessor"
+  member    = google_service_account.runtime_sso_dashboard_dev.member
 }
 
 resource "google_secret_manager_secret_iam_member" "runtime_sso_dashboard_dev_oidc_client_secret" {
   secret_id = data.google_secret_manager_secret.sso_dashboard_dev_oidc_client_secret.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = google_service_account.runtime_sso_dashboard_dev.member
+  role      = "roles/secretmanager.secretAccessor"
+  member    = google_service_account.runtime_sso_dashboard_dev.member
 }
 
 resource "google_service_account" "runtime_sso_dashboard_stage" {
-  account_id                   = "sso-dashboard-stage-runtime"
-  display_name                 = "SSO Dashboard Stage (runtime)"
+  account_id   = "sso-dashboard-stage-runtime"
+  display_name = "SSO Dashboard Stage (runtime)"
 }
 
 resource "google_secret_manager_secret_iam_member" "runtime_sso_dashboard_stage_secret_key" {
   secret_id = data.google_secret_manager_secret.sso_dashboard_prod_secret_key.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = google_service_account.runtime_sso_dashboard_stage.member
+  role      = "roles/secretmanager.secretAccessor"
+  member    = google_service_account.runtime_sso_dashboard_stage.member
 }
 
 resource "google_secret_manager_secret_iam_member" "runtime_sso_dashboard_stage_oidc_client_secret" {
   secret_id = data.google_secret_manager_secret.sso_dashboard_prod_oidc_client_secret.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = google_service_account.runtime_sso_dashboard_stage.member
+  role      = "roles/secretmanager.secretAccessor"
+  member    = google_service_account.runtime_sso_dashboard_stage.member
 }
 
 resource "google_service_account" "runtime_sso_dashboard_prod" {
-  account_id                   = "sso-dashboard-prod-runtime"
-  display_name                 = "SSO Dashboard Prod (runtime)"
+  account_id   = "sso-dashboard-prod-runtime"
+  display_name = "SSO Dashboard Prod (runtime)"
 }
 
 resource "google_secret_manager_secret_iam_member" "runtime_sso_dashboard_prod_secret_key" {
   secret_id = data.google_secret_manager_secret.sso_dashboard_prod_secret_key.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = google_service_account.runtime_sso_dashboard_prod.member
+  role      = "roles/secretmanager.secretAccessor"
+  member    = google_service_account.runtime_sso_dashboard_prod.member
 }
 
 resource "google_secret_manager_secret_iam_member" "runtime_sso_dashboard_prod_oidc_client_secret" {
   secret_id = data.google_secret_manager_secret.sso_dashboard_prod_oidc_client_secret.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = google_service_account.runtime_sso_dashboard_prod.member
+  role      = "roles/secretmanager.secretAccessor"
+  member    = google_service_account.runtime_sso_dashboard_prod.member
 }
