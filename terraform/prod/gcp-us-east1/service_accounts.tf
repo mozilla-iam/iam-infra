9f64beb1dd6ef480bdefcc23105e1834efe58cd9
Bheesham Persaud <bhee@mozilla.com>
2025-10-22 16:10:59 -0400

    Fixup typo
    Jira: IAM-1824

--- terraform/prod/gcp-us-east1/service_accounts.tf
+++ terraform/prod/gcp-us-east1/service_accounts.tf
@@ -9,13 +9,13 @@ resource "google_service_account" "sso_dashboard" {
 
 resource "google_service_account" "sso_dashboard_staging" {
   account_id                   = "sso-dashboard-staging"
-  description                  = "A dedicated service account for sso-dashboard-staging cloud run serivce"
+  description                  = "A dedicated service account for sso-dashboard-staging cloud run service"
   display_name                 = "sso-dashboard-staging"
 }
 
 resource "google_service_account" "sso_dashboard_prod" {
   account_id                   = "sso-dashboard-prod"
-  description                  = "A dedicated service account for sso-dashboard-prod cloud run serivce "
+  description                  = "A dedicated service account for sso-dashboard-prod cloud run service"
   display_name                 = "sso-dashboard-prod"
 }
 
