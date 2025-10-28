48304d6e92b4d3877d12a976f68396cf76b0c4e6
Bheesham Persaud <bhee@mozilla.com>
2025-10-28 16:11:05 -0400

    fix(gcp): Prep for creating a new SSO Dashboard endpoint
    Jira: IAM-1653

--- terraform/prod/gcp-us-east1/imports.tf
+++ terraform/prod/gcp-us-east1/imports.tf
@@ -42,3 +42,53 @@ import {
   id = "projects/iam-auth0/serviceAccounts/sso-dashboard-prod@iam-auth0.iam.gserviceaccount.com"
   to = google_service_account.sso_dashboard_prod
 }
+
+import {
+  id = "projects/iam-auth0/global/sslCertificates/sso-dashboard-dev"
+  to = google_compute_managed_ssl_certificate.sso_dashboard_dev
+}
+
+import {
+  id = "projects/iam-auth0/global/forwardingRules/sso-dashboard-dev"
+  to = google_compute_global_forwarding_rule.sso_dashboard_dev
+}
+
+import {
+  id = "projects/iam-auth0/global/addresses/sso-dashboard-dev"
+  to = google_compute_global_address.sso_dashboard_dev
+}
+
+import {
+  id = "projects/iam-auth0/global/targetHttpsProxies/sso-dashboard-dev-target-proxy"
+  to = google_compute_target_https_proxy.sso_dashboard_dev
+}
+
+import {
+  id = "projects/iam-auth0/global/urlMaps/sso-dashboard-dev"
+  to = google_compute_url_map.sso_dashboard_dev
+}
+
+import {
+  id = "projects/iam-auth0/global/sslCertificates/sso-dashboard-prod"
+  to = google_compute_managed_ssl_certificate.sso_dashboard_prod
+}
+
+import {
+  id = "projects/iam-auth0/global/forwardingRules/sso-dashboard"
+  to = google_compute_global_forwarding_rule.sso_dashboard_prod
+}
+
+import {
+  id = "projects/iam-auth0/global/addresses/sso-dashboard"
+  to = google_compute_global_address.sso_dashboard_prod
+}
+
+import {
+  id = "projects/iam-auth0/global/targetHttpsProxies/sso-dashboard-target-proxy"
+  to = google_compute_target_https_proxy.sso_dashboard_prod
+}
+
+import {
+  id = "projects/iam-auth0/global/urlMaps/sso-dashboard"
+  to = google_compute_url_map.sso_dashboard_prod
+}
