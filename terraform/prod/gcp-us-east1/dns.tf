d37a0a0d56d18e87443866ab2da45d322bcd6d75
Bheesham Persaud <bhee@mozilla.com>
2025-10-29 21:13:25 -0400

    feat(gcp): We need an intermediate domain between Fastly and the SSO Dashboard
    Jira: IAM-1653

--- terraform/prod/gcp-us-east1/dns.tf
+++ terraform/prod/gcp-us-east1/dns.tf
@@ -10,6 +10,14 @@ resource "google_dns_managed_zone" "gcp_iam_auth0" {
   visibility = "public"
 }
 
+resource "google_dns_record_set" "waf_sso_dashboard_dev" {
+  name         = "waf-sso-dashboard-dev.gcp-iam-auth0.sso.mozilla.com."
+  managed_zone = google_dns_managed_zone.gcp_iam_auth0.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.sso_dashboard_dev.address]
+}
+
 # We'll need the following outputs to be copied over to our AWS Hosted Zone.
 
 output "gcp_iam_auth0_dns_zone_ns_records" {
