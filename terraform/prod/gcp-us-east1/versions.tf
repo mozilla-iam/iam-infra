b43044094169e4f46e172857358c1a8d9747423b
Bheesham Persaud <bhee@mozilla.com>
2025-11-03 13:34:13 -0500

    chore(tf/gcp): upgrade deps (terraform + google provider)
    Jira: IAM-1653

--- terraform/prod/gcp-us-east1/versions.tf
+++ terraform/prod/gcp-us-east1/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = "~> 1.13"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = "~> 7.0"
     }
   }
 }
